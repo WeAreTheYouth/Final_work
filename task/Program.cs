5ebd2627c3c9824b3e33a8b2de3739e2795d76a0
WeAreTheYouth <open8your5mind@gmail.com>
2022-10-16 01:28:22 +0300

    add task

---
+++ task/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array = { "hello", "2", "world", ":-)", "1234", "1567", "-2" };
+string[] newArray = new string[array.Length];
+MethodArray(array);
+PrintArray(newArray);
+
+void MethodArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[i] = array[i];
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
